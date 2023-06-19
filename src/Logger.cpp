e3e3eac0b7d4c7a79f7dcb8c397885efd785bd52
BackZhao <backzhao@qq.com>
2023-06-19 09:35:47 +0800

    Logger: 添加FIXME(daemon下无法刷新日志)

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -34,5 +34,7 @@ bool Logger::Init(spdlog::level::level_enum level, const std::string& logFile, s
     m_logger->set_level(level);
     m_logger->set_pattern(pattern);
 
+    // FIXME: daemon下无法刷新日志文件
+
     return true;
 }
